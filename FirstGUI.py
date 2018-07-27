1b5a221e9d745b9ef9e1f00453b2dc35a27a386b
Petr Stenin <petr.stenin@gmail.com>
2018-07-27 23:44:04 +1000

    Added a button and associated action

--- FirstGUI.py
+++ FirstGUI.py
@@ -1,6 +1,21 @@
 import tkinter as tk
+from tkinter import ttk
 
 win = tk.Tk()
 win.title('Python GUI')
-win.resizable(0, 0)
+
+# Add a label
+a_label = ttk.Label(win, text='A Label')
+a_label.grid(column=0, row=0)
+
+# Button click event function
+def click_me():
+    action.configure(text='@@ I have been clicked @@')
+    a_label.configure(foreground='red')
+    a_label.configure(text='A Red Label')
+
+# Add a button
+action = ttk.Button(win, text='Click me!', command=click_me)
+action.grid(column=1, row=0)
+
 win.mainloop()
