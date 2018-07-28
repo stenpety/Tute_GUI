e3e6507864159bb5744d988c03b5fd485e58052f
Petr Stenin <petr.stenin@gmail.com>
2018-07-28 23:53:54 +1000

    Added a combo box

--- FirstGUI.py
+++ FirstGUI.py
@@ -8,22 +8,32 @@ win.title('Python GUI')
 a_label = ttk.Label(win, text='Enter your name: ')
 a_label.grid(column=0, row=0)
 
+
 # Button click event function
 def click_me():
-    action.configure(text='Hello ' + name.get())
+    action.configure(text='Hello ' + name.get() + ' ' + )
 
 
 # Textbox entry
 name = tk.StringVar()
 name_entered = ttk.Entry(win, width=12, textvariable=name)
-name_entered.grid(column=1, row=0)
+name_entered.grid(column=0, row=1)
+name_entered.focus()  # Gives focus to text box
 
 
 # Add a button
 action = ttk.Button(win, text='Click me!', command=click_me)
-action.grid(column=1, row=1)
+action.grid(column=2, row=1)
+# action.configure(state='disabled')  # Disables button
 
 
+# Add a combo box
+ttk.Label(win, text='Choose a number: ').grid(column=1, row=0)
+number = tk.StringVar()
+number_chosen = ttk.Combobox(win, width=12, textvariable=number, state='readonly')
+number_chosen['values'] = (1, 2, 4, 42, 100)
+number_chosen.grid(column=1, row=1)
+number_chosen.current(0)
 
 
 win.mainloop()
